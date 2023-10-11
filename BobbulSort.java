33912a1ea33d42cb709d57e493b45012d7df5ae7
Deepak Mewada <mewadadeepak367@gmail.com>
2023-10-12 00:00:43 +0530

    made Cycsl sort

---
+++ BobbulSort.java
@@ -0,0 +1,8 @@
+public class BobbulSort{
+    public static void main(String[] args) {
+        
+    }
+    static int[] bobbulSort(int[] arr){
+        return arr;
+    }
+}
---
+++ CycalSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class CycalSort{
+    public static void main(String[] args) {
+        int[] arr={4,1,2,5,3,7,6};
+        System.out.println(Arrays.toString(cycalSort(arr)));
+    }
+    static int[] cycalSort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        return arr;
+    } 
+    static void swap(int[] arr,int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
+}
